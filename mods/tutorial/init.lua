7501174bcdd59181fbd8063b875a015d8cabf340
Der1248 <1248.kontakt@gmail.com>
2021-04-15 13:37:16 +0200

    Regnum 2 1.3.1 upload

--- mods/tutorial/init.lua
+++ mods/tutorial/init.lua
@@ -14,7 +14,7 @@ minetest.register_on_joinplayer(function(player)
 		offset = {x=0, y=30},
 		alignment = {x=1, y=0},
 		number = 0xFFFFFF ,
-		text = "Game Version	 :  1.3.0",
+		text = "Game Version	 :  1.3.1",
 	})
 end)
 
